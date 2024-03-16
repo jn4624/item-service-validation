33fcf6bae7974c482f57174fb42b6fdcb85d1b4e
LimJiNa <jn4624@gmail.com>
2024-03-16 22:15:27 +0900

    Initial commit

---
+++ src/main/java/hello/item/ItemServiceValidationApplication.java
@@ -0,0 +1,13 @@
+package hello.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/item/ItemServiceValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
