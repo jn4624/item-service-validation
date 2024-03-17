117765a31e984bcbc45a4d29489302761b7fcca2
LimJiNa <jn4624@gmail.com>
2024-03-17 12:05:09 +0900

    Validation - 오류 코드와 메시지 처리2 (rejectValue(), reject()) v2

--- src/main/java/hello/item/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/item/web/validation/ValidationItemControllerV2.java
@@ -122,19 +122,62 @@ public class ValidationItemControllerV2 {
     /**
      * 오류 코드와 메시지 처리1
      */
+//    @PostMapping("/add")
+//    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+//        // 검증 로직
+//        if (!StringUtils.hasText(item.getItemName())) {
+//            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName"}, null, null));
+//        }
+//
+//        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+//            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1000, 1000000}, null));
+//        }
+//
+//        if (item.getQuantity() == null || item.getQuantity() > 9999) {
+//            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, new String[]{"max.item.quantity"}, new Object[]{9999}, null));
+//        }
+//
+//        // 특정 필드가 아닌 복합 룰 검증
+//        if (item.getPrice() != null && item.getQuantity() != null) {
+//            int resultPrice = item.getPrice() * item.getQuantity();
+//
+//            if (resultPrice < 10000) {
+//                bindingResult.addError(new ObjectError("item", new String[]{"total.price.min"}, new Object[]{10000, resultPrice}, null));
+//            }
+//        }
+//
+//        // 검증에 실패하면 다시 입력 폼으로
+//        if (bindingResult.hasErrors()) {
+//            log.info("errors = {}", bindingResult);
+//            return "validation/v2/addForm";
+//        }
+//
+//        // 정상 로직
+//        Item savedItem = itemRepository.save(item);
+//        redirectAttributes.addAttribute("itemId", savedItem.getId());
+//        redirectAttributes.addAttribute("status", true);
+//        return "redirect:/validation/v2/items/{itemId}";
+//    }
+
+    /**
+     * 오류 코드와 메시지 처리2
+     */
     @PostMapping("/add")
-    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        log.info("objectName = {}", bindingResult.getObjectName());
+        log.info("target = {}", bindingResult.getTarget());
+
         // 검증 로직
         if (!StringUtils.hasText(item.getItemName())) {
-            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName"}, null, null));
+            bindingResult.rejectValue("itemName", "required");
         }
 
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1000, 1000000}, null));
+            bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
         }
 
         if (item.getQuantity() == null || item.getQuantity() > 9999) {
-            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, new String[]{"max.item.quantity"}, new Object[]{9999}, null));
+            bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
         }
 
         // 특정 필드가 아닌 복합 룰 검증
@@ -142,7 +185,7 @@ public class ValidationItemControllerV2 {
             int resultPrice = item.getPrice() * item.getQuantity();
 
             if (resultPrice < 10000) {
-                bindingResult.addError(new ObjectError("item", new String[]{"total.price.min"}, new Object[]{10000, resultPrice}, null));
+                bindingResult.reject("total", new Object[]{10000, resultPrice}, null);
             }
         }
 
