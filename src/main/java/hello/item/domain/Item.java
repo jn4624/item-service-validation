9f8b6e8167ca617edd2f105a7a412f23d01f26b3
LimJiNa <jn4624@gmail.com>
2024-03-17 22:29:52 +0900

    Bean Validation - Form 전송 객체 분리 v4

--- src/main/java/hello/item/domain/Item.java
+++ src/main/java/hello/item/domain/Item.java
@@ -1,25 +1,21 @@
 package hello.item.domain;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import org.hibernate.validator.constraints.Range;
 
 @Data
 public class Item {
-    @NotNull(groups = UpdateCheck.class) // 상품 수정 - 요구사항 추가
+//    @NotNull(groups = UpdateCheck.class) // 상품 수정 - 요구사항 추가
     private Long id;
 
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = SaveCheck.class) // 상품 수정 - 요구사항 추가
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = SaveCheck.class) // 상품 수정 - 요구사항 추가
     private Integer quantity;
 
     public Item() {
